ba781746615c83ca17ba1e07e280036a510c6eab
Aashubro <aashutoshbro1@gmail.com>
2023-06-04 21:00:32 +0545

    added mp3 player play and stop button

--- ap.java
+++ ap.java
@@ -1,11 +1,35 @@
-import java.awt.*;
-import java.applet.*;
-public class ap extends Applet
-{
- public void paint(Graphics g)
-{
- g.drawOval(20,20,200,120);
- g.setColor(Color.green);
- g.fillOval(70,30,100,100);
-}
+import java.applet.Applet;
+import java.awt.Button;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.applet.AudioClip;
+public class ap extends Applet implements ActionListener {
+    private Button playButton;
+    private Button stopButton;
+    private AudioClip audioClip;
+
+    public void init() {
+        playButton = new Button("Play");
+        playButton.addActionListener(this);
+        add(playButton);
+        stopButton = new Button("Stop");
+        stopButton.addActionListener(this);
+        add(stopButton);
+        try {
+            // Replace the URL with the path to your audio file
+            URL audioURL = new URL(getCodeBase(), "bhajan.mp3");
+            audioClip = getAudioClip(audioURL);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == playButton) {
+            audioClip.play();
+        } else if (e.getSource() == stopButton) {
+            audioClip.stop();
+        }
+    }
 }
