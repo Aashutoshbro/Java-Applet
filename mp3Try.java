aee66d58206f7ac82c11dd07ffdf4f6efd1daf47
Aashubro <aashutoshbro1@gmail.com>
2023-06-10 22:41:55 +0545

    convert applet to application

---
+++ mp3Try.java
@@ -0,0 +1,35 @@
+import java.applet.Applet;
+import java.awt.Button;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.applet.AudioClip;
+public class mp3Try extends Applet implements ActionListener {
+    private Button playButton;
+    private Button stopButton;
+    private AudioClip audioClip;
+
+    public void init() {
+        playButton = new Button("Play");
+        playButton.addActionListener(this);
+        add(playButton);
+        stopButton = new Button("Stop");
+        stopButton.addActionListener(this);
+        add(stopButton);
+        try {
+            // Replace the URL with the path to your audio file
+            URL audioURL = new URL(getCodeBase(), "bhajan.wav");
+            audioClip = getAudioClip(audioURL);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == playButton) {
+            audioClip.play();
+        } else if (e.getSource() == stopButton) {
+            audioClip.stop();
+        }
+    }
+}
